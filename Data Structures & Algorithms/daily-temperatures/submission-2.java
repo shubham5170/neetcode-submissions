82992f298dcb3d765c0d803952da10f80358e2bf
shubham5170 <83545837+shubham5170@users.noreply.github.com>
2026-07-10 10:02:16 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,30 @@
+/*
+Solution to this lies in finding most immediate max temp and once you find it not care about that number at all. 
+So just maintain a stack keeping all the numbers while iterating array.
+For each iteration, go through stack and see all the numbers where temp was smaller than this number, you have find answer
+So remove that number from stack and do it for all elements in stack. Once done push current number to stack. 
+Time complexity is O(n) as you are going through array just once. There is no extra complexity for stack as we just top elemnts or we find our answer
+*/
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res =  new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<n; i++)
+        {
+            while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[i])
+            {
+                int cooler = stack.pop();
+                res[cooler] = i-cooler;
+            }
+            stack.push(i);
+
+        }
+
+        return res;
+
+        
+    }
+}
