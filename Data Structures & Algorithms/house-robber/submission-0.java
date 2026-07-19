7609bb9eedc37a945af7ca39f975269e2e19f748
shubham5170 <83545837+shubham5170@users.noreply.github.com>
2026-07-19 13:47:52 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp =  new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;i++)
+        {
+            dp[i]=Math.max(nums[i-1]+dp[i-2],dp[i-1]);
+        }
+        return dp[n];
+    }
+}
